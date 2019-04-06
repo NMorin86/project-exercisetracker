fa460c313026ce5e8831505dd70397803cf55ca5
Glitch (nmorin86-project-exercisetracker) <none>
2019-04-06 22:02:14 +0000

    ⭐️🎑 Checkpoint ./server.js:3676277/349 ./exercise.js:3676277/689

---
+++ exercise.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const loggerSchema = mongoose.Schema({
+  
+
+function postNewUser(err, req, res, next) {
+  
+}
+
+function getAllUsers(err, req, res, next) {
+  
+}
+
+function postNewExercise(err, req, res, next) {
+  
+}
+
+function getExerciseLog(err, req, res, next) {
+  
+}
+
+
+
+module.exports = { postNewUser: postNewUser,
+                   getAllUsers: getAllUsers,
+                   postNewExercise: postNewExercise,
+                   getExerciseLog: getExerciseLog
+                 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const ex = require('./exercise.js');
 
 const cors = require('cors')
 
@@ -43,26 +44,27 @@ app.use((err, req, res, next) => {
     .send(errMessage)
 })
 
+//    I can create a user by posting form data username to /api/exercise/new-user 
+//    and returned will be an object with username and _id.
+app.post('/api/exercise/new-user', ex.postNewUser);
 
-//    I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
-app.post('/api/exercise/new-user', 
+//    I can get an array of all users by getting api/exercise/users with the same 
+//    info as when creating a user.
+app.get('/api/exercise/users', ex.getAllUsers);
 
-//    I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+//    I can add an exercise to any user by posting form data userId(_id), 
+//    description, duration, and optionally date to /api/exercise/add. If no date 
+//    supplied it will use current date. Returned will the the user object with 
+//    also with the exercise fields added.
+app.post('/api/exercise/add', ex.postNewExercise);
 
+//    I can retrieve a full exercise log of any user by getting /api/exercise/log 
+//    with a parameter of userId(_id). Return will be the user object with added 
+//    array log and count (total exercise count).
+app.get('/api/exercise/log', ex.getExerciseLog);
 
-//    I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. Returned will the the user object with also with the exercise fields added.
-    
-
-//    I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
-    
-
-//    I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
-
-
-
-
-
-
+//    I can retrieve part of the log of any user by also passing along optional 
+//    parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
